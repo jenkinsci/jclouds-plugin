dcd841297b738c968c3fbcbc8832586da0790908
Adrian Cole <adrian@jclouds.org>
2012-04-30 14:01:22 -0700

    added test examples for compute and blobstore

---
+++ src/test/java/jenkins/plugins/jclouds/compute/ComputeTestFixture.java
@@ -0,0 +1,92 @@
+package jenkins.plugins.jclouds.compute;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Map;
+
+import org.jclouds.compute.ComputeService;
+import org.jclouds.compute.internal.BaseComputeServiceContextLiveTest;
+import org.jclouds.sshj.config.SshjSshClientModule;
+import org.jclouds.util.Maps2;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Maps;
+import com.google.inject.Module;
+
+@SuppressWarnings("unchecked")
+public class ComputeTestFixture extends BaseComputeServiceContextLiveTest {
+   public static String PROVIDER;
+
+   /**
+    * base jclouds tests expect properties to arrive in a different naming convention, based on
+    * provider name.
+    * 
+    * ex.
+    * 
+    * <pre>
+    *  test.jenkins.compute.provider=aws-ec2
+    *  test.jenkins.compute.identity=access
+    *  test.jenkins.compute.credential=secret
+    * </pre>
+    * 
+    * should turn into
+    * 
+    * <pre>
+    *  test.aws-ec2.identity=access
+    *  test.aws-ec2.credential=secret
+    * </pre>
+    */
+   static {
+      PROVIDER = checkNotNull(System.getProperty("test.jenkins.compute.provider"),
+               "test.compute.provider variable must be set!");
+      Map<String, String> filtered = Maps.filterKeys(Map.class.cast(System.getProperties()), Predicates
+               .containsPattern("^test\\.jenkins\\.compute"));
+      Map<String, String> transformed = Maps2.transformKeys(filtered, new Function<String, String>() {
+
+         public String apply(String arg0) {
+            return arg0.replaceAll("test.jenkins.compute", "test." + PROVIDER);
+         }
+
+      });
+      System.getProperties().putAll(transformed);
+   }
+
+   public ComputeTestFixture() {
+      provider = PROVIDER;
+   }
+
+   @Override
+   protected Module getSshModule() {
+      return new SshjSshClientModule();
+   }
+
+   public ComputeService getComputeService() {
+      return view.getComputeService();
+   }
+
+   public String getProvider() {
+      return provider;
+   }
+
+   public String getEndpoint() {
+      return endpoint;
+   }
+
+   public String getIdentity() {
+      return identity;
+   }
+
+   public String getCredential() {
+      return credential;
+   }
+
+   public void setUp() {
+      super.setupContext();
+   }
+
+   public void tearDown() {
+      super.tearDownContext();
+   }
+
+}
---
+++ src/test/java/jenkins/plugins/jclouds/compute/JCloudsCloudLiveTest.java
@@ -0,0 +1,54 @@
+package jenkins.plugins.jclouds.compute;
+
+import hudson.util.FormValidation;
+
+import java.util.Collections;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.jclouds.crypto.SshKeys;
+
+public class JCloudsCloudLiveTest extends TestCase {
+
+   private ComputeTestFixture fixture;
+   private JCloudsCloud cloud;
+   private Map<String, String> generatedKeys;
+
+   @Override
+   public void setUp() throws Exception {
+      super.setUp();
+      fixture = new ComputeTestFixture();
+      fixture.setUp();
+      generatedKeys = SshKeys.generate();
+      
+      // TODO: this may need to vary per test
+      cloud = new JCloudsCloud(
+               fixture.getProvider() + "-profile", 
+               fixture.getProvider(), 
+               fixture.getIdentity(),
+               fixture.getCredential(), 
+               generatedKeys.get("private"), 
+               generatedKeys.get("public"), 
+               fixture.getEndpoint(), 
+               1, 
+               30, 
+               Collections.<JCloudsSlaveTemplate> emptyList());
+   }
+
+   public void testDoTestConnectionCorrectCredentialsEtc() {
+      FormValidation result = new JCloudsCloud.DescriptorImpl().doTestConnection(
+                                    fixture.getProvider(),
+                                    fixture.getIdentity(),
+                                    fixture.getCredential(), 
+                                    generatedKeys.get("private"),
+                                    fixture.getEndpoint());
+      assertEquals("Connection succeeded!", result.getMessage());
+   }
+
+   @Override
+   public void tearDown() {
+      if (fixture != null)
+         fixture.tearDown();
+   }
+}
