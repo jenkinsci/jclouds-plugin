769aee080e874ed7bd3432508c1c180b4bc8b665
Andrew Bayer <andrew.bayer@gmail.com>
2012-05-20 10:09:35 -0700

    Allow logging of jclouds.wire if jenkins.plugins.jclouds.compute.ComputeLogger.wireLogging=true

--- src/main/java/jenkins/plugins/jclouds/compute/ComputeLogger.java
+++ src/main/java/jenkins/plugins/jclouds/compute/ComputeLogger.java
@@ -9,26 +9,28 @@ import java.util.logging.Logger;
  */
 class ComputeLogger extends org.jclouds.logging.jdk.JDKLogger {
 
-   public static class Factory extends JDKLoggerFactory {
-      public org.jclouds.logging.Logger getLogger(String category) {
-         if (category.equals("jclouds.compute"))
-            return new ComputeLogger(Logger.getLogger(category));
-         else
-            return super.getLogger(category);
-      }
-   }
-
-   public ComputeLogger(Logger logger) {
-      super(logger);
-   }
-
-   @Override
-   public boolean isDebugEnabled() {
-      return true;
-   }
-
-   @Override
-   protected void logDebug(String message) {
-      super.logInfo(message);
-   }
+    public static class Factory extends JDKLoggerFactory {
+        public org.jclouds.logging.Logger getLogger(String category) {
+            if (category.equals("jclouds.compute") || (category.equals("jclouds.wire") && wireLogging))  
+                return new ComputeLogger(Logger.getLogger(category));
+            else
+                return super.getLogger(category);
+        }
+    }
+    
+    public ComputeLogger(Logger logger) {
+        super(logger);
+    }
+    
+    @Override
+    public boolean isDebugEnabled() {
+        return true;
+    }
+    
+    @Override
+    protected void logDebug(String message) {
+        super.logInfo(message);
+    }
+    
+    public static boolean wireLogging = Boolean.getBoolean(ComputeLogger.class.getName()+".wireLogging");
 }
