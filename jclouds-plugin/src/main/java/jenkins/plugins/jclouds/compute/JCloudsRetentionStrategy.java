d2e11aa7a99ab2815618ff32976d8b70c7308c8f
Fritz Elfert <fritz@fritz-elfert.de>
2015-09-17 22:58:33 +0200

    Do not interrupt any wait for phone home

--- jclouds-plugin/src/main/java/jenkins/plugins/jclouds/compute/JCloudsRetentionStrategy.java
+++ jclouds-plugin/src/main/java/jenkins/plugins/jclouds/compute/JCloudsRetentionStrategy.java
@@ -39,7 +39,6 @@ public class JCloudsRetentionStrategy extends RetentionStrategy<JCloudsComputer>
                             if (!c.isOffline()) {
                                 c.setTemporarilyOffline(true, OfflineCause.create(Messages._DeletedCause()));
                             }
-                            node.setPendingDelete(true);
                         }
                     }
                 }
