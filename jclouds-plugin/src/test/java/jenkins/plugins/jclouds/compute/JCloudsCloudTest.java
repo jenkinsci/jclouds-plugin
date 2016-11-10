4170abbd43673c8cef93ba5be2b310ec090b2cb6
Fritz Elfert <fritz@fritz-elfert.de>
2016-11-10 22:09:05 +0100

    Fixed GUI test

--- jclouds-plugin/src/test/java/jenkins/plugins/jclouds/compute/JCloudsCloudTest.java
+++ jclouds-plugin/src/test/java/jenkins/plugins/jclouds/compute/JCloudsCloudTest.java
@@ -23,15 +23,6 @@ public class JCloudsCloudTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
-    private void savePage(final HtmlPage p, final String name) {
-        try {
-            final java.io.PrintStream ps = new java.io.PrintStream(new java.io.FileOutputStream(name));
-            ps.println(p.asXml());
-            ps.close();
-        } catch (java.io.IOException e) {
-        }
-    }
-
     private void mySelectPresent(final HtmlPage p, final String name) {
         final String xpath = "//select[@name='" + name + "']";
         final List<?> list = p.getByXPath(xpath);
@@ -42,53 +33,28 @@ public class JCloudsCloudTest {
 
     @Test
     public void testConfigurationUI() throws Exception {
-        j.recipeLoadCurrentPlugin();
-        j.configRoundtrip();
+        JCloudsCloud cloud = new JCloudsCloud("aws-profile", "aws-ec2", "",
+                "", "http://localhost", 1, CloudInstanceDefaults.DEFAULT_INSTANCE_RETENTION_TIME_IN_MINUTES,
+                600 * 1000, 600 * 1000, null, true, Collections.<JCloudsSlaveTemplate>emptyList());
+        j.getInstance().clouds.add(cloud);
 
         HtmlPage p = j.createWebClient().goTo("configure");
-
-        final String pageText = p.asText();
-        assertTrue("Cloud Section must be present in the global configuration ", pageText.contains("Cloud"));
-
-        // savePage(p, "page0.html");
-        HtmlForm f = p.getFormByName("config");
-        HtmlButton b = HtmlFormUtil.getButtonByCaption(f, "Add a new cloud");
-        p = b.click();
-        // savePage(p, "page1.html");
-        WebAssert.assertLinkPresentWithText(p, "Cloud (JClouds)");
-        /* new HtmlUnit somehow does not invoke the JClouds menu entry
-         *
-        savePage(p, "page2.html");
-        HtmlAnchor a = p.getAnchorByText("Cloud (JClouds)");
-        // p = a.click();
-        p = (HtmlPage)a.mouseOver();
-        a = p.getAnchorByText("Cloud (JClouds)");
-        savePage(p, "page3.html");
-        p = (HtmlPage)a.mouseDown();
-        savePage(p, "page4.html");
-        a = p.getAnchorByText("Cloud (JClouds)");
-        p = (HtmlPage)a.mouseUp();
-        savePage(p, "page5.html");
-
         WebAssert.assertInputPresent(p, "_.profile");
+        mySelectPresent(p, "_.providerName");
         WebAssert.assertInputPresent(p, "_.endPointUrl");
-        // WebAssert does not recognize select as input ?!
-        mySelectPresent(p, "_.cloudCredentialsId");
         WebAssert.assertInputPresent(p, "_.instanceCap");
         WebAssert.assertInputPresent(p, "_.retentionTime");
-        // WebAssert does not recognize select as input ?!
+        mySelectPresent(p, "_.cloudCredentialsId");
+        WebAssert.assertInputPresent(p, "_.trustAll");
         mySelectPresent(p, "_.cloudGlobalKeyId");
         WebAssert.assertInputPresent(p, "_.scriptTimeout");
         WebAssert.assertInputPresent(p, "_.startTimeout");
         WebAssert.assertInputPresent(p, "_.zones");
-        WebAssert.assertInputPresent(p, "_.trustAll");
-
-        f = p.getFormByName("config");
-        b = HtmlFormUtil.getButtonByCaption(f, "Test Connection");
+        HtmlForm f = p.getFormByName("config");
+        HtmlButton b = HtmlFormUtil.getButtonByCaption(f, "Test Connection");
         assertNotNull(b);
         b = HtmlFormUtil.getButtonByCaption(f, "Delete cloud");
         assertNotNull(b);
-        */
     }
 
     @Test
