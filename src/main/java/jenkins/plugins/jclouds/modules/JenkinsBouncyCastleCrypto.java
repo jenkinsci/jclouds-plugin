035bf8ecd0a112ab02a053d8b63442180f737031
Basil Crow <me@basilcrow.com>
2024-01-09 14:15:50 -0800

    Forward compatibility with Guice 7

--- src/main/java/jenkins/plugins/jclouds/modules/JenkinsBouncyCastleCrypto.java
+++ src/main/java/jenkins/plugins/jclouds/modules/JenkinsBouncyCastleCrypto.java
@@ -7,7 +7,7 @@ import java.security.cert.CertificateException;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
-import javax.inject.Singleton;
+import jakarta.inject.Singleton;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jclouds.crypto.Crypto;
